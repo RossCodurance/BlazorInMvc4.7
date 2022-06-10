b44f473e86907e1b45a9b9a5362894f010be84df
RossCodurance <ross.waddell@codurance.com>
2022-06-10 09:00:01 +0100

    MVC and Blazor projects running together
    4.7 MVC site which loads in components from a Blazor project.
    
    Operates in 2 methods: loading on page load, and loading via dynamically generated HTML and JS

---
+++ BlazorApp/Program.cs
@@ -0,0 +1,14 @@
+using BlazorApp;
+using BlazorApp.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.RootComponents.RegisterForJavaScript<TestComponent>(identifier: "TestComponent", javaScriptInitializer: "loadTestComponent");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
